4f0555564f8c7c505c2e901f970e9d621ad3e2af
aleev97 <valverdealejandro327@gmail.com>
2023-03-30 09:00:23 -0300

    commit responsive LP

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React from 'react';
 import Styles from './Landing.module.css';
-import image from './imag/ParaPIdogs.webp';
+import image from './images/ParaPIdogs.webp';
 import { useNavigate } from 'react-router-dom';
 
 export default function LandingPage() {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-function backToHome() {
-    navigate("/home");
+  function backToHome() {
+    navigate('/home');
   }
-    return (
-        <div className={Styles.container}>
-            <img className={Styles.img} src={image} alt="" />
-                <h1 className={Styles.title} >Welcome the dogs app</h1>
-                <button onClick={backToHome} className={Styles.button} >Enter Home</button>
-            <h2 className={Styles.text} >
-                This is a web page made in order for you to get to know your faithful companion better.
-                You will be able to see breeds, temperaments and more data about the dog you choose.
-            </h2>
-        </div>
-    )
-}    
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <div className={Styles.container}>
+        <img className={Styles.img} src={image} alt="" />
+        <h1 className={Styles.title}>Welcome to the dogs app</h1>
+        <button onClick={backToHome} className={Styles.button}>
+          Enter Home
+        </button>
+        <h2 className={Styles.text}>
+          This is a web page made in order for you to get to know your faithful
+          companion better. You will be able to see breeds, temperaments and more
+          data about the dog you choose.
+        </h2>
+      </div>
+    </>
+  );
+}
